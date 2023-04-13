a52e23494905a0dc92b9c9922cee4642f598ffc8
saintmatthieu <saintmatthieu@gmail.com>
2023-04-13 12:26:09 +0200

    All working

--- saint/tempo-detection-eval/main.cpp
+++ saint/tempo-detection-eval/main.cpp
@@ -2,6 +2,7 @@
 #include "ProgressBar.h"
 #include "WavFileReader.h"
 
+#include <cassert>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -9,6 +10,20 @@
 
 namespace fs = std::filesystem;
 using namespace std::literals::string_literals;
+
+std::string getSolutionName(saint::BpmEstimator::Solution solution) {
+  switch (solution) {
+  case saint::BpmEstimator::Solution::MiniBpm:
+    return "MiniBpm";
+  default:
+    assert(false);
+    return "";
+  }
+}
+
+template <typename T> std::string valueOrEmptyStr(const std::optional<T> &opt) {
+  return opt.has_value() ? std::to_string(static_cast<int>(*opt)) : "";
+}
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -49,21 +64,29 @@ int main(int argc, char **argv) {
     bool falsePositive = false;
     bool falseNegative = false;
     std::optional<float> error;
+    fs::path file;
   };
 
   std::map<Solution, std::vector<Result>> results;
 
-  saint::ProgressBar progress(files.size());
+  saint::ProgressBar detectionProgress(files.size());
   std::cout << "Running tempo detection on " << files.size() << " files"
             << std::endl;
+  std::ofstream resultCsv{"tempo-detection-eval.csv"};
+  resultCsv
+      << "solution, file, expected, actual, error, falsePositive, falseNegative"
+      << std::endl;
   for (auto i = 0u; i < files.size(); ++i) {
-    progress.increment();
+    detectionProgress.increment();
     saint::WavFileReader reader(files[i]);
     const auto sr = reader.getSampleRate();
     const auto actualBpm = tempi[i];
     constexpr auto numSolutions = static_cast<int>(Solution::_count);
     for (auto j = 0; j < numSolutions; ++j) {
       const auto solution = static_cast<Solution>(j);
+      if (j == 0) {
+        results[solution].reserve(files.size());
+      }
       const auto estimator = saint::BpmEstimator::createInstance(solution, sr);
       constexpr auto blockSize = 512;
       while (reader.getNumSamplesPerChannelAvailable() > 0) {
@@ -84,7 +107,14 @@ int main(int argc, char **argv) {
       if (actualBpm.has_value() && estimate.has_value()) {
         result.error = *estimate - *actualBpm;
       }
+      result.file = files[i];
       results[solution].emplace_back(std::move(result));
+      resultCsv << getSolutionName(solution) << ", "
+                << files[i].filename().string() << ", "
+                << valueOrEmptyStr(actualBpm) << ", "
+                << valueOrEmptyStr(estimate) << ", "
+                << valueOrEmptyStr(result.error) << ", " << result.falsePositive
+                << ", " << result.falseNegative << std::endl;
     }
   }
 
